5a650386c87283c70e9598eeeb4dbe1b36a4fe56
Csaba Gyuris <gycsaba@vasvari.hu>
2019-03-11 15:10:27 +0100

    Feladat megoldásra kész

---
+++ GUIOOPRadioButtonCheckboxTeleKocsi/modell/TeleKocsi.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUIOOPRadioButtonCheckboxTeleKocsi.modell
+{
+    enum Uticel { DEBRECEN, BUDAPEST, GYOR};
+
+    class TeleKocsi
+    {
+        static int ferohely = 4;
+        private Uticel uticel;
+        private bool kedvezmeny;
+        private int utasokSzama;
+
+        public TeleKocsi()
+        {
+            uticel = Uticel.BUDAPEST;
+            kedvezmeny = false;
+            utasokSzama = 0;
+        }
+
+        public TeleKocsi(Uticel uticel, bool kedvezmeny, int utasokSzama)
+        {
+            this.uticel = uticel;
+            this.kedvezmeny = kedvezmeny;
+            this.utasokSzama = utasokSzama;
+        }
+
+        public void modositUticeltBudapestre()
+        { }
+        public void modositUticelGyor()
+        { }
+        public void modositUticelDebrecen()
+        { }
+        public void valtoztatKedvezmenyt()
+        { }
+        public void modositUtasokSzama(int utasokSzama)
+        { }
+        public int szamolAr()
+        { return 0;}
+    }
+}
