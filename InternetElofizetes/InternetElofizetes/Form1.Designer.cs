297dc79d62d7ca3cab6f3fec90ff8fa87baa358b
Csaba Gyuris <gycsaba@vasvari.hu>
2019-03-14 11:07:28 +0100

    Feladat view kész

---
+++ InternetElofizetes/InternetElofizetes/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace InternetElofizetes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxLetoltesiSebesseg = new System.Windows.Forms.GroupBox();
+            this.radioButtonSebesseg100 = new System.Windows.Forms.RadioButton();
+            this.radioButtonSebesseg200 = new System.Windows.Forms.RadioButton();
+            this.radioButtonSebesseg50 = new System.Windows.Forms.RadioButton();
+            this.radioButton50 = new System.Windows.Forms.RadioButton();
+            this.radioButtonSebesseg10 = new System.Windows.Forms.RadioButton();
+            this.labelFelirat = new System.Windows.Forms.Label();
+            this.groupBoxLehetosegek = new System.Windows.Forms.GroupBox();
+            this.checkBoxVezetekesTelefon = new System.Windows.Forms.CheckBox();
+            this.checkBoxIPTV = new System.Windows.Forms.CheckBox();
+            this.checkBoxKorlatlanLetoltes = new System.Windows.Forms.CheckBox();
+            this.numericUpDownElofizetesekSzama = new System.Windows.Forms.NumericUpDown();
+            this.labelElofizeteesekSzama = new System.Windows.Forms.Label();
+            this.labelFizetesiOsszeg = new System.Windows.Forms.Label();
+            this.textBoxFizetesiOsszeg = new System.Windows.Forms.TextBox();
+            this.buttonKilepes = new System.Windows.Forms.Button();
+            this.groupBoxLetoltesiSebesseg.SuspendLayout();
+            this.groupBoxLehetosegek.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownElofizetesekSzama)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxLetoltesiSebesseg
+            // 
+            this.groupBoxLetoltesiSebesseg.Controls.Add(this.radioButtonSebesseg100);
+            this.groupBoxLetoltesiSebesseg.Controls.Add(this.radioButtonSebesseg200);
+            this.groupBoxLetoltesiSebesseg.Controls.Add(this.radioButtonSebesseg50);
+            this.groupBoxLetoltesiSebesseg.Controls.Add(this.radioButton50);
+            this.groupBoxLetoltesiSebesseg.Controls.Add(this.radioButtonSebesseg10);
+            this.groupBoxLetoltesiSebesseg.Location = new System.Drawing.Point(16, 50);
+            this.groupBoxLetoltesiSebesseg.Name = "groupBoxLetoltesiSebesseg";
+            this.groupBoxLetoltesiSebesseg.Size = new System.Drawing.Size(200, 130);
+            this.groupBoxLetoltesiSebesseg.TabIndex = 0;
+            this.groupBoxLetoltesiSebesseg.TabStop = false;
+            this.groupBoxLetoltesiSebesseg.Text = "Letöltési sebesség";
+            // 
+            // radioButtonSebesseg100
+            // 
+            this.radioButtonSebesseg100.AutoSize = true;
+            this.radioButtonSebesseg100.Location = new System.Drawing.Point(22, 40);
+            this.radioButtonSebesseg100.Name = "radioButtonSebesseg100";
+            this.radioButtonSebesseg100.Size = new System.Drawing.Size(76, 17);
+            this.radioButtonSebesseg100.TabIndex = 4;
+            this.radioButtonSebesseg100.TabStop = true;
+            this.radioButtonSebesseg100.Text = "100 Mbit/s";
+            this.radioButtonSebesseg100.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonSebesseg200
+            // 
+            this.radioButtonSebesseg200.AutoSize = true;
+            this.radioButtonSebesseg200.Location = new System.Drawing.Point(22, 19);
+            this.radioButtonSebesseg200.Name = "radioButtonSebesseg200";
+            this.radioButtonSebesseg200.Size = new System.Drawing.Size(76, 17);
+            this.radioButtonSebesseg200.TabIndex = 3;
+            this.radioButtonSebesseg200.TabStop = true;
+            this.radioButtonSebesseg200.Text = "200 Mbit/s";
+            this.radioButtonSebesseg200.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonSebesseg50
+            // 
+            this.radioButtonSebesseg50.AutoSize = true;
+            this.radioButtonSebesseg50.Location = new System.Drawing.Point(22, 63);
+            this.radioButtonSebesseg50.Name = "radioButtonSebesseg50";
+            this.radioButtonSebesseg50.Size = new System.Drawing.Size(70, 17);
+            this.radioButtonSebesseg50.TabIndex = 2;
+            this.radioButtonSebesseg50.TabStop = true;
+            this.radioButtonSebesseg50.Text = "50 Mbit/s";
+            this.radioButtonSebesseg50.UseVisualStyleBackColor = true;
+            // 
+            // radioButton50
+            // 
+            this.radioButton50.AutoSize = true;
+            this.radioButton50.Location = new System.Drawing.Point(22, 86);
+            this.radioButton50.Name = "radioButton50";
+            this.radioButton50.Size = new System.Drawing.Size(70, 17);
+            this.radioButton50.TabIndex = 1;
+            this.radioButton50.TabStop = true;
+            this.radioButton50.Text = "50 Mbit/s";
+            this.radioButton50.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonSebesseg10
+            // 
+            this.radioButtonSebesseg10.AutoSize = true;
+            this.radioButtonSebesseg10.Location = new System.Drawing.Point(22, 109);
+            this.radioButtonSebesseg10.Name = "radioButtonSebesseg10";
+            this.radioButtonSebesseg10.Size = new System.Drawing.Size(70, 17);
+            this.radioButtonSebesseg10.TabIndex = 0;
+            this.radioButtonSebesseg10.TabStop = true;
+            this.radioButtonSebesseg10.Text = "10 Mbit/s";
+            this.radioButtonSebesseg10.UseVisualStyleBackColor = true;
+            // 
+            // labelFelirat
+            // 
+            this.labelFelirat.AutoSize = true;
+            this.labelFelirat.Location = new System.Drawing.Point(13, 13);
+            this.labelFelirat.Name = "labelFelirat";
+            this.labelFelirat.Size = new System.Drawing.Size(157, 13);
+            this.labelFelirat.TabIndex = 1;
+            this.labelFelirat.Text = "Internet szolgáltatás kalkulácsió";
+            // 
+            // groupBoxLehetosegek
+            // 
+            this.groupBoxLehetosegek.Controls.Add(this.checkBoxVezetekesTelefon);
+            this.groupBoxLehetosegek.Controls.Add(this.checkBoxIPTV);
+            this.groupBoxLehetosegek.Controls.Add(this.checkBoxKorlatlanLetoltes);
+            this.groupBoxLehetosegek.Location = new System.Drawing.Point(280, 50);
+            this.groupBoxLehetosegek.Name = "groupBoxLehetosegek";
+            this.groupBoxLehetosegek.Size = new System.Drawing.Size(200, 130);
+            this.groupBoxLehetosegek.TabIndex = 2;
+            this.groupBoxLehetosegek.TabStop = false;
+            this.groupBoxLehetosegek.Text = "Egyéb lehetőségek";
+            // 
+            // checkBoxVezetekesTelefon
+            // 
+            this.checkBoxVezetekesTelefon.AutoSize = true;
+            this.checkBoxVezetekesTelefon.Location = new System.Drawing.Point(24, 66);
+            this.checkBoxVezetekesTelefon.Name = "checkBoxVezetekesTelefon";
+            this.checkBoxVezetekesTelefon.Size = new System.Drawing.Size(111, 17);
+            this.checkBoxVezetekesTelefon.TabIndex = 2;
+            this.checkBoxVezetekesTelefon.Text = "Vezetékes telefon";
+            this.checkBoxVezetekesTelefon.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxIPTV
+            // 
+            this.checkBoxIPTV.AutoSize = true;
+            this.checkBoxIPTV.Location = new System.Drawing.Point(24, 43);
+            this.checkBoxIPTV.Name = "checkBoxIPTV";
+            this.checkBoxIPTV.Size = new System.Drawing.Size(53, 17);
+            this.checkBoxIPTV.TabIndex = 1;
+            this.checkBoxIPTV.Text = "IP TV";
+            this.checkBoxIPTV.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxKorlatlanLetoltes
+            // 
+            this.checkBoxKorlatlanLetoltes.AutoSize = true;
+            this.checkBoxKorlatlanLetoltes.Location = new System.Drawing.Point(24, 20);
+            this.checkBoxKorlatlanLetoltes.Name = "checkBoxKorlatlanLetoltes";
+            this.checkBoxKorlatlanLetoltes.Size = new System.Drawing.Size(103, 17);
+            this.checkBoxKorlatlanLetoltes.TabIndex = 0;
+            this.checkBoxKorlatlanLetoltes.Text = "Korlátlan letöltés";
+            this.checkBoxKorlatlanLetoltes.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDownElofizetesekSzama
+            // 
+            this.numericUpDownElofizetesekSzama.Location = new System.Drawing.Point(163, 221);
+            this.numericUpDownElofizetesekSzama.Name = "numericUpDownElofizetesekSzama";
+            this.numericUpDownElofizetesekSzama.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDownElofizetesekSzama.TabIndex = 3;
+            // 
+            // labelElofizeteesekSzama
+            // 
+            this.labelElofizeteesekSzama.AutoSize = true;
+            this.labelElofizeteesekSzama.Location = new System.Drawing.Point(13, 228);
+            this.labelElofizeteesekSzama.Name = "labelElofizeteesekSzama";
+            this.labelElofizeteesekSzama.Size = new System.Drawing.Size(100, 13);
+            this.labelElofizeteesekSzama.TabIndex = 4;
+            this.labelElofizeteesekSzama.Text = "Előfizetések száma:";
+            // 
+            // labelFizetesiOsszeg
+            // 
+            this.labelFizetesiOsszeg.AutoSize = true;
+            this.labelFizetesiOsszeg.Location = new System.Drawing.Point(14, 265);
+            this.labelFizetesiOsszeg.Name = "labelFizetesiOsszeg";
+            this.labelFizetesiOsszeg.Size = new System.Drawing.Size(125, 13);
+            this.labelFizetesiOsszeg.TabIndex = 5;
+            this.labelFizetesiOsszeg.Text = "Kiszámolt fizetési összeg:";
+            // 
+            // textBoxFizetesiOsszeg
+            // 
+            this.textBoxFizetesiOsszeg.Location = new System.Drawing.Point(163, 258);
+            this.textBoxFizetesiOsszeg.Name = "textBoxFizetesiOsszeg";
+            this.textBoxFizetesiOsszeg.ReadOnly = true;
+            this.textBoxFizetesiOsszeg.Size = new System.Drawing.Size(121, 20);
+            this.textBoxFizetesiOsszeg.TabIndex = 6;
+            // 
+            // buttonKilepes
+            // 
+            this.buttonKilepes.Location = new System.Drawing.Point(405, 298);
+            this.buttonKilepes.Name = "buttonKilepes";
+            this.buttonKilepes.Size = new System.Drawing.Size(75, 23);
+            this.buttonKilepes.TabIndex = 7;
+            this.buttonKilepes.Text = "Kilépés";
+            this.buttonKilepes.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 369);
+            this.Controls.Add(this.buttonKilepes);
+            this.Controls.Add(this.textBoxFizetesiOsszeg);
+            this.Controls.Add(this.labelFizetesiOsszeg);
+            this.Controls.Add(this.labelElofizeteesekSzama);
+            this.Controls.Add(this.numericUpDownElofizetesekSzama);
+            this.Controls.Add(this.groupBoxLehetosegek);
+            this.Controls.Add(this.labelFelirat);
+            this.Controls.Add(this.groupBoxLetoltesiSebesseg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBoxLetoltesiSebesseg.ResumeLayout(false);
+            this.groupBoxLetoltesiSebesseg.PerformLayout();
+            this.groupBoxLehetosegek.ResumeLayout(false);
+            this.groupBoxLehetosegek.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownElofizetesekSzama)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxLetoltesiSebesseg;
+        private System.Windows.Forms.RadioButton radioButtonSebesseg100;
+        private System.Windows.Forms.RadioButton radioButtonSebesseg200;
+        private System.Windows.Forms.RadioButton radioButtonSebesseg50;
+        private System.Windows.Forms.RadioButton radioButton50;
+        private System.Windows.Forms.RadioButton radioButtonSebesseg10;
+        private System.Windows.Forms.Label labelFelirat;
+        private System.Windows.Forms.GroupBox groupBoxLehetosegek;
+        private System.Windows.Forms.CheckBox checkBoxVezetekesTelefon;
+        private System.Windows.Forms.CheckBox checkBoxIPTV;
+        private System.Windows.Forms.CheckBox checkBoxKorlatlanLetoltes;
+        private System.Windows.Forms.NumericUpDown numericUpDownElofizetesekSzama;
+        private System.Windows.Forms.Label labelElofizeteesekSzama;
+        private System.Windows.Forms.Label labelFizetesiOsszeg;
+        private System.Windows.Forms.TextBox textBoxFizetesiOsszeg;
+        private System.Windows.Forms.Button buttonKilepes;
+    }
+}
+
