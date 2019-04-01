b335d270ecfbdab2be0a82d2d98009f8ba428e2b
Csaba Gyuris <gycsaba@vasvari.hu>
2019-04-01 12:38:14 +0200

    Fagyi GUI kész

---
+++ GUIOOPRadioButtonCheckBoxFagyi/GUIOOPRadioButtonCheckBoxFagyi/FormFagyi.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace GUIOOPRadioButtonCheckBoxFagyi
+{
+    partial class FormFagyi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButtonEdesTolcser = new System.Windows.Forms.RadioButton();
+            this.radioButtonGlutenMentesTolcser = new System.Windows.Forms.RadioButton();
+            this.radioButtonKicsiTolcser = new System.Windows.Forms.RadioButton();
+            this.checkBoxGyumolcsRatet = new System.Windows.Forms.CheckBox();
+            this.checkBoxEdesRud = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.labelGombocokSzama = new System.Windows.Forms.Label();
+            this.labelFizetendoOsszeg = new System.Windows.Forms.Label();
+            this.textBoxFizetendoOsszeg = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButtonEdesTolcser
+            // 
+            this.radioButtonEdesTolcser.AutoSize = true;
+            this.radioButtonEdesTolcser.Location = new System.Drawing.Point(12, 44);
+            this.radioButtonEdesTolcser.Name = "radioButtonEdesTolcser";
+            this.radioButtonEdesTolcser.Size = new System.Drawing.Size(82, 17);
+            this.radioButtonEdesTolcser.TabIndex = 0;
+            this.radioButtonEdesTolcser.TabStop = true;
+            this.radioButtonEdesTolcser.Text = "édes tölcsér";
+            this.radioButtonEdesTolcser.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonGlutenMentesTolcser
+            // 
+            this.radioButtonGlutenMentesTolcser.AutoSize = true;
+            this.radioButtonGlutenMentesTolcser.Location = new System.Drawing.Point(12, 67);
+            this.radioButtonGlutenMentesTolcser.Name = "radioButtonGlutenMentesTolcser";
+            this.radioButtonGlutenMentesTolcser.Size = new System.Drawing.Size(125, 17);
+            this.radioButtonGlutenMentesTolcser.TabIndex = 1;
+            this.radioButtonGlutenMentesTolcser.TabStop = true;
+            this.radioButtonGlutenMentesTolcser.Text = "glutén mentes tölcsér";
+            this.radioButtonGlutenMentesTolcser.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonKicsiTolcser
+            // 
+            this.radioButtonKicsiTolcser.AutoSize = true;
+            this.radioButtonKicsiTolcser.Location = new System.Drawing.Point(12, 90);
+            this.radioButtonKicsiTolcser.Name = "radioButtonKicsiTolcser";
+            this.radioButtonKicsiTolcser.Size = new System.Drawing.Size(80, 17);
+            this.radioButtonKicsiTolcser.TabIndex = 2;
+            this.radioButtonKicsiTolcser.TabStop = true;
+            this.radioButtonKicsiTolcser.Text = "kicsi tölcsér";
+            this.radioButtonKicsiTolcser.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxGyumolcsRatet
+            // 
+            this.checkBoxGyumolcsRatet.AutoSize = true;
+            this.checkBoxGyumolcsRatet.Location = new System.Drawing.Point(167, 44);
+            this.checkBoxGyumolcsRatet.Name = "checkBoxGyumolcsRatet";
+            this.checkBoxGyumolcsRatet.Size = new System.Drawing.Size(94, 17);
+            this.checkBoxGyumolcsRatet.TabIndex = 3;
+            this.checkBoxGyumolcsRatet.Text = "gyümölcs rátét";
+            this.checkBoxGyumolcsRatet.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxEdesRud
+            // 
+            this.checkBoxEdesRud.AutoSize = true;
+            this.checkBoxEdesRud.Location = new System.Drawing.Point(167, 68);
+            this.checkBoxEdesRud.Name = "checkBoxEdesRud";
+            this.checkBoxEdesRud.Size = new System.Drawing.Size(67, 17);
+            this.checkBoxEdesRud.TabIndex = 4;
+            this.checkBoxEdesRud.Text = "édes rúd";
+            this.checkBoxEdesRud.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(114, 136);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(97, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // labelGombocokSzama
+            // 
+            this.labelGombocokSzama.AutoSize = true;
+            this.labelGombocokSzama.Location = new System.Drawing.Point(13, 138);
+            this.labelGombocokSzama.Name = "labelGombocokSzama";
+            this.labelGombocokSzama.Size = new System.Drawing.Size(95, 13);
+            this.labelGombocokSzama.TabIndex = 6;
+            this.labelGombocokSzama.Text = "Gombócok száma:";
+            // 
+            // labelFizetendoOsszeg
+            // 
+            this.labelFizetendoOsszeg.AutoSize = true;
+            this.labelFizetendoOsszeg.Location = new System.Drawing.Point(13, 178);
+            this.labelFizetendoOsszeg.Name = "labelFizetendoOsszeg";
+            this.labelFizetendoOsszeg.Size = new System.Drawing.Size(92, 13);
+            this.labelFizetendoOsszeg.TabIndex = 7;
+            this.labelFizetendoOsszeg.Text = "Fizetendő összeg:";
+            // 
+            // textBoxFizetendoOsszeg
+            // 
+            this.textBoxFizetendoOsszeg.Location = new System.Drawing.Point(111, 178);
+            this.textBoxFizetendoOsszeg.Name = "textBoxFizetendoOsszeg";
+            this.textBoxFizetendoOsszeg.ReadOnly = true;
+            this.textBoxFizetendoOsszeg.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFizetendoOsszeg.TabIndex = 8;
+            this.textBoxFizetendoOsszeg.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // FormFagyi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBoxFizetendoOsszeg);
+            this.Controls.Add(this.labelFizetendoOsszeg);
+            this.Controls.Add(this.labelGombocokSzama);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBoxEdesRud);
+            this.Controls.Add(this.checkBoxGyumolcsRatet);
+            this.Controls.Add(this.radioButtonKicsiTolcser);
+            this.Controls.Add(this.radioButtonGlutenMentesTolcser);
+            this.Controls.Add(this.radioButtonEdesTolcser);
+            this.Name = "FormFagyi";
+            this.Text = "Fagyi rendelés";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButtonEdesTolcser;
+        private System.Windows.Forms.RadioButton radioButtonGlutenMentesTolcser;
+        private System.Windows.Forms.RadioButton radioButtonKicsiTolcser;
+        private System.Windows.Forms.CheckBox checkBoxGyumolcsRatet;
+        private System.Windows.Forms.CheckBox checkBoxEdesRud;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label labelGombocokSzama;
+        private System.Windows.Forms.Label labelFizetendoOsszeg;
+        private System.Windows.Forms.TextBox textBoxFizetendoOsszeg;
+    }
+}
+
--- GUIOOPRadioButtonCheckBoxFagyi/GUIOOPRadioButtonCheckBoxFagyi/FormFagyi.cs
+++ GUIOOPRadioButtonCheckBoxFagyi/GUIOOPRadioButtonCheckBoxFagyi/FormFagyi.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GUIOOPRadioButtonCheckBoxFagyi
 {
-    public partial class Form1 : Form
+    public partial class FormFagyi : Form
     {
-        public Form1()
+        public FormFagyi()
         {
             InitializeComponent();
         }
